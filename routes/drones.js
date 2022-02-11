46722b363670db534ded20db4c1cbff187f3f6c3
Pablo López <p.lopezmunoz@gmail.com>
2022-02-11 18:12:49 +0100

    1st commit

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+  .then((drones)=>{
+    console.log(`Drones are displayed in the list`)
+    res.render('drones/list', {drones})
+  })
+  .catch(error=>`Error while loading drones: ${error}`)
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.route('/drones/create')
+.get((req, res, next) => {
+  res.render('drones/create-form')
+})
+.post((req, res, next) => {
+  const name = req.body.name;
+  const propellers = req.body.propellers;
+  const maxSpeed = req.body.maxSpeed;
+  Drone.create({name, propellers, maxSpeed})
+  .then(()=>{res.redirect('/drones')})
+  .catch((error)=>{
+    console.log(`Error while loading drones: ${error}`);
+    res.redirect('/drones/create')
+  }    
+  )
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,28 @@
-// Iteration #1
+require('../db/index');
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model')
+
+const droneArmy = [
+    {
+        name: "Turbokiller",
+        propellers: 5,
+        maxSpeed: 15
+    },
+    {
+        name: "Kikidronix",
+        propellers: 3,
+        maxSpeed: 8
+    },
+    {
+        name: "Satana",
+        propellers: 4,
+        maxSpeed: 20
+    }
+]
+
+Drone.create(droneArmy)
+.then(dronesFromDb => {
+  console.log(`Added ${dronesFromDb.length} drones`);
+  mongoose.connection.close();
+})
+.catch(err => console.log(`An error occurred while creating drones: ${err}`))
